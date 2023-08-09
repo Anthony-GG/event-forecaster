5bbaf2daeb3b4cce5621a8e489ad421862bce349
Anthony Iacano <iacano5@outlook.com>
2023-08-08 20:58:49 -0400

    added way to pull icon for current weather status, working on displaying to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+var forecast_image = document.getElementById("#forecast-image")
+
+
 const OPENWEATHER_API_KEY = "6ddb7b9eda44e747c0962325870a6579";
 
 getInfo("http://api.openweathermap.org/data/2.5/weather?q=Cleveland,us&APPID=6ddb7b9eda44e747c0962325870a6579")
@@ -6,5 +9,12 @@ async function getInfo(file) {
     var myObject = await fetch(file);
     console.log(myObject);
     var myText = await myObject.text();
-    console.log(JSON.parse(myText));
+    var weather_data = JSON.parse(myText);
+    console.log(weather_data);
+
+    var icon_link = "http://openweathermap.org/img/w/" + weather_data.weather[0].icon + ".png";
+    let img = new Image();
+    img.src = icon_link;
+    console.log(img);
+    forecast_image.appendChild(img);
 }
