42f5eb63367afd99615ced65d6837fb6be32ecad
Anthony Iacano <iacano5@outlook.com>
2023-08-08 20:45:53 -0400

    set openweather api call to cleveland for testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 const OPENWEATHER_API_KEY = "6ddb7b9eda44e747c0962325870a6579";
 
-getInfo("http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=6ddb7b9eda44e747c0962325870a6579")
+getInfo("http://api.openweathermap.org/data/2.5/weather?q=Cleveland,us&APPID=6ddb7b9eda44e747c0962325870a6579")
 
 async function getInfo(file) {
     var myObject = await fetch(file);
     console.log(myObject);
     var myText = await myObject.text();
-    console.log(myText);
+    console.log(JSON.parse(myText));
 }
